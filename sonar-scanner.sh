094f587792a6c39ffa9d85ebffa0258a38105604
Axel Ilali <axel.ilali@yahoo.fr>
2022-11-24 12:12:36 +0100

    ci: add sonarqube config

---
+++ sonar-scanner.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=github-actions-test \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=sqp_d0bbba61d841430d8d7625961941ecfca6cabee3
